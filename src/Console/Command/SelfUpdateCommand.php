PHP-CS-Fixer/PHP-CS-Fixer

Add PregWrapper

src/Console/Command/SelfUpdateCommand.php
@@ -14,6 +14,7 @@ namespace PhpCsFixer\Console\Command;
 
 use PhpCsFixer\Console\SelfUpdate\NewVersionCheckerInterface;
 use PhpCsFixer\PharCheckerInterface;
+use PhpCsFixer\PregWrapper;
 use PhpCsFixer\ToolInfoInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -99,7 +100,7 @@ EOT
         }
 
         $currentVersion = $this->getApplication()->getVersion();
-        preg_match('/^v?(?<major>\d+)\./', $currentVersion, $matches);
+        PregWrapper::match('/^v?(?<major>\d+)\./', $currentVersion, $matches);
         $currentMajor = (int) $matches['major'];
 
         try {

src/DocBlock/Line.php
@@ -12,6 +12,8 @@
 
 namespace PhpCsFixer\DocBlock;
 
+use PhpCsFixer\PregWrapper;
+
 /**
  * This represents a line of a docblock.
  *
@@ -65,7 +67,7 @@ class Line
      */
     public function containsUsefulContent()
     {
-        return 0 !== preg_match('/\\*\s*\S+/', $this->content) && !$this->isTheStart() && !$this->isTheEnd();
+        return 0 !== PregWrapper::match('/\\*\s*\S+/', $this->content) && !$this->isTheStart() && !$this->isTheEnd();
     }
 
     /**
@@ -77,7 +79,7 @@ class Line
      */
     public function containsATag()
     {
-        return 0 !== preg_match('/\\*\s*@/', $this->content);
+        return 0 !== PregWrapper::match('/\\*\s*@/', $this->content);
     }
 
     /**
@@ -131,7 +133,7 @@ class Line
      */
     public function addBlank()
     {
-        $matched = preg_match('/^([ \t]*\*)[^\r\n]*(\r?\n)$/', $this->content, $matches);
+        $matched = PregWrapper::match('/^([ \t]*\*)[^\r\n]*(\r?\n)$/', $this->content, $matches);
 
         if (1 !== $matched) {
             return;

src/Doctrine/Annotation/Tokens.php
@@ -13,6 +13,7 @@
 namespace PhpCsFixer\Doctrine\Annotation;
 
 use Doctrine\Common\Annotations\DocLexer;
+use PhpCsFixer\PregWrapper;
 use PhpCsFixer\Tokenizer\Token as PhpToken;
 
 /**
@@ -42,7 +43,7 @@ final class Tokens extends \SplFixedArray
         $ignoredTextPosition = 0;
         $currentPosition = 0;
         while (false !== $nextAtPosition = strpos($content, '@', $currentPosition)) {
-            if (0 !== $nextAtPosition && !preg_match('/\s/', $content[$nextAtPosition - 1])) {
+            if (0 !== $nextAtPosition && !PregWrapper::match('/\s/', $content[$nextAtPosition - 1])) {
                 $currentPosition = $nextAtPosition + 1;
 
                 continue;
@@ -197,7 +198,7 @@ final class Tokens extends \SplFixedArray
                 isset($this[$index + 3])
                 && $this[$index + 2]->isType(DocLexer::T_NONE)
                 && $this[$index + 3]->isType(DocLexer::T_OPEN_PARENTHESIS)
-                && preg_match('/^(\R\s*\*\s*)*\s*$/', $this[$index + 2]->getContent())
+                && PregWrapper::match('/^(\R\s*\*\s*)*\s*$/', $this[$index + 2]->getContent())
             ) {
                 $currentIndex = $index + 3;
             }

src/Fixer/DoctrineAnnotation/DoctrineAnnotationSpacesFixer.php
@@ -21,6 +21,7 @@ use PhpCsFixer\FixerConfiguration\FixerConfigurationResolver;
 use PhpCsFixer\FixerConfiguration\FixerOptionBuilder;
 use PhpCsFixer\FixerDefinition\CodeSample;
 use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\PregWrapper;
 
 /**
  * Fixes spaces around commas and assignment operators in Doctrine annotations.
@@ -204,7 +205,7 @@ final class DoctrineAnnotationSpacesFixer extends AbstractDoctrineAnnotationFixe
                 $token->clear();
             }
 
-            if ($index < count($tokens) - 1 && !preg_match('/^\s/', $tokens[$index + 1]->getContent())) {
+            if ($index < count($tokens) - 1 && !PregWrapper::match('/^\s/', $tokens[$index + 1]->getContent())) {
                 $tokens->insertAt($index + 1, new Token(DocLexer::T_NONE, ' '));
             }
         }

src/Fixer/PhpUnit/PhpUnitFqcnAnnotationFixer.php
@@ -15,6 +15,7 @@ namespace PhpCsFixer\Fixer\PhpUnit;
 use PhpCsFixer\AbstractFixer;
 use PhpCsFixer\FixerDefinition\CodeSample;
 use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\PregWrapper;
 use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
 
@@ -73,7 +74,7 @@ final class MyTest extends \PHPUnit_Framework_TestCase
     {
         foreach ($tokens as $index => $token) {
             if ($token->isGivenKind(T_DOC_COMMENT)) {
-                $tokens[$index] = new Token(array(T_DOC_COMMENT, preg_replace(
+                $tokens[$index] = new Token(array(T_DOC_COMMENT, PregWrapper::replace(
                     '~^(\s*\*\s*@(?:expectedException|covers|coversDefaultClass|uses)\h+)(\w.*)$~m',
                     '$1\\\\$2',
                     $token->getContent()

src/Fixer/Phpdoc/PhpdocIndentFixer.php
@@ -15,6 +15,7 @@ namespace PhpCsFixer\Fixer\Phpdoc;
 use PhpCsFixer\AbstractFixer;
 use PhpCsFixer\FixerDefinition\CodeSample;
 use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\PregWrapper;
 use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
 use PhpCsFixer\Utils;
@@ -123,7 +124,7 @@ class DocBlocks
      */
     private function fixDocBlock($content, $indent)
     {
-        return ltrim(preg_replace('/^[ \t]*\*/m', $indent.' *', $content));
+        return ltrim(PregWrapper::replace('/^[ \t]*\*/m', $indent.' *', $content));
     }
 
     /**

tests/AutoReview/ProjectCodeTest.php
@@ -14,6 +14,7 @@ namespace PhpCsFixer\Tests\AutoReview;
 
 use PhpCsFixer\DocBlock\DocBlock;
 use PhpCsFixer\Tests\TestCase;
+use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
@@ -252,6 +253,38 @@ final class ProjectCodeTest extends TestCase
         ));
     }
 
+    /**
+     * @dataProvider provideClassesWherePregFunctionsAreForbiddenCases
+     *
+     * @param string $className
+     */
+    public function testThereIsNoPregFunctionUsedDirectly($className)
+    {
+        $rc = new \ReflectionClass($className);
+        $tokens = Tokens::fromCode(file_get_contents($rc->getFileName()));
+        $stringTokens = array_filter(
+            $tokens->toArray(),
+            function (Token $token) {
+                return $token->isGivenKind(T_STRING);
+            }
+        );
+        $strings = array_map(
+            function (Token $token) {
+                return $token->getContent();
+            },
+            $stringTokens
+        );
+        $strings = array_unique($strings);
+        $message = sprintf('Tested file: %s', $rc->getFileName());
+        $this->assertNotContains('preg_filter', $strings, $message);
+        $this->assertNotContains('preg_grep', $strings, $message);
+        $this->assertNotContains('preg_match', $strings, $message);
+        $this->assertNotContains('preg_match_all', $strings, $message);
+        $this->assertNotContains('preg_replace', $strings, $message);
+        $this->assertNotContains('preg_replace_callback', $strings, $message);
+        $this->assertNotContains('preg_split', $strings, $message);
+    }
+
     public function provideSrcClassCases()
     {
         return array_map(
@@ -359,6 +392,25 @@ final class ProjectCodeTest extends TestCase
         }
 
         return $data;
+    }
+
+    public function provideClassesWherePregFunctionsAreForbiddenCases()
+    {
+        if (extension_loaded('xdebug') && false === getenv('CI')) {
+            $this->markTestSkipped('Data provider too slow when Xdebug is loaded.');
+        }
+
+        return array_map(
+            function ($item) {
+                return array($item);
+            },
+            array_filter(
+                $this->getSrcClasses(),
+                function ($className) {
+                    return 'PhpCsFixer\\PregWrapper' !== $className;
+                }
+            )
+        );
     }
 
     private function getSrcClasses()

karolswiderski-0817/php-version-update-code

src/Console/Command/SelfUpdateCommand.php
@@ -14,6 +14,7 @@ namespace PhpCsFixer\Console\Command;
 
 use PhpCsFixer\Console\SelfUpdate\NewVersionCheckerInterface;
 use PhpCsFixer\PharCheckerInterface;
+use PhpCsFixer\Preg;
 use PhpCsFixer\ToolInfoInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -99,7 +100,7 @@ EOT
         }
 
         $currentVersion = $this->getApplication()->getVersion();
-        preg_match('/^v?(?<major>\d+)\./', $currentVersion, $matches);
+        Preg::match('/^v?(?<major>\d+)\./', $currentVersion, $matches);
         $currentMajor = (int) $matches['major'];
 
         try {

src/DocBlock/Line.php
@@ -12,6 +12,8 @@
 
 namespace PhpCsFixer\DocBlock;
 
+use PhpCsFixer\Preg;
+
 /**
  * This represents a line of a docblock.
  *
@@ -65,7 +67,7 @@ class Line
      */
     public function containsUsefulContent()
     {
-        return 0 !== preg_match('/\\*\s*\S+/', $this->content) && !$this->isTheStart() && !$this->isTheEnd();
+        return 0 !== Preg::match('/\\*\s*\S+/', $this->content) && !$this->isTheStart() && !$this->isTheEnd();
     }
 
     /**
@@ -77,7 +79,7 @@ class Line
      */
     public function containsATag()
     {
-        return 0 !== preg_match('/\\*\s*@/', $this->content);
+        return 0 !== Preg::match('/\\*\s*@/', $this->content);
     }
 
     /**
@@ -131,7 +133,7 @@ class Line
      */
     public function addBlank()
     {
-        $matched = preg_match('/^([ \t]*\*)[^\r\n]*(\r?\n)$/', $this->content, $matches);
+        $matched = Preg::match('/^([ \t]*\*)[^\r\n]*(\r?\n)$/', $this->content, $matches);
 
         if (1 !== $matched) {
             return;

src/Doctrine/Annotation/Tokens.php
@@ -13,6 +13,7 @@
 namespace PhpCsFixer\Doctrine\Annotation;
 
 use Doctrine\Common\Annotations\DocLexer;
+use PhpCsFixer\Preg;
 use PhpCsFixer\Tokenizer\Token as PhpToken;
 
 /**
@@ -42,7 +43,7 @@ final class Tokens extends \SplFixedArray
         $ignoredTextPosition = 0;
         $currentPosition = 0;
         while (false !== $nextAtPosition = strpos($content, '@', $currentPosition)) {
-            if (0 !== $nextAtPosition && !preg_match('/\s/', $content[$nextAtPosition - 1])) {
+            if (0 !== $nextAtPosition && !Preg::match('/\s/', $content[$nextAtPosition - 1])) {
                 $currentPosition = $nextAtPosition + 1;
 
                 continue;
@@ -197,7 +198,7 @@ final class Tokens extends \SplFixedArray
                 isset($this[$index + 3])
                 && $this[$index + 2]->isType(DocLexer::T_NONE)
                 && $this[$index + 3]->isType(DocLexer::T_OPEN_PARENTHESIS)
-                && preg_match('/^(\R\s*\*\s*)*\s*$/', $this[$index + 2]->getContent())
+                && Preg::match('/^(\R\s*\*\s*)*\s*$/', $this[$index + 2]->getContent())
             ) {
                 $currentIndex = $index + 3;
             }

src/Fixer/DoctrineAnnotation/DoctrineAnnotationSpacesFixer.php
@@ -21,6 +21,7 @@ use PhpCsFixer\FixerConfiguration\FixerConfigurationResolver;
 use PhpCsFixer\FixerConfiguration\FixerOptionBuilder;
 use PhpCsFixer\FixerDefinition\CodeSample;
 use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\Preg;
 
 /**
  * Fixes spaces around commas and assignment operators in Doctrine annotations.
@@ -204,7 +205,7 @@ final class DoctrineAnnotationSpacesFixer extends AbstractDoctrineAnnotationFixe
                 $token->clear();
             }
 
-            if ($index < count($tokens) - 1 && !preg_match('/^\s/', $tokens[$index + 1]->getContent())) {
+            if ($index < count($tokens) - 1 && !Preg::match('/^\s/', $tokens[$index + 1]->getContent())) {
                 $tokens->insertAt($index + 1, new Token(DocLexer::T_NONE, ' '));
             }
         }

src/Fixer/PhpUnit/PhpUnitFqcnAnnotationFixer.php
@@ -15,6 +15,7 @@ namespace PhpCsFixer\Fixer\PhpUnit;
 use PhpCsFixer\AbstractFixer;
 use PhpCsFixer\FixerDefinition\CodeSample;
 use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\Preg;
 use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
 
@@ -73,7 +74,7 @@ final class MyTest extends \PHPUnit_Framework_TestCase
     {
         foreach ($tokens as $index => $token) {
             if ($token->isGivenKind(T_DOC_COMMENT)) {
-                $tokens[$index] = new Token(array(T_DOC_COMMENT, preg_replace(
+                $tokens[$index] = new Token(array(T_DOC_COMMENT, Preg::replace(
                     '~^(\s*\*\s*@(?:expectedException|covers|coversDefaultClass|uses)\h+)(\w.*)$~m',
                     '$1\\\\$2',
                     $token->getContent()

src/Fixer/Phpdoc/PhpdocIndentFixer.php
@@ -15,6 +15,7 @@ namespace PhpCsFixer\Fixer\Phpdoc;
 use PhpCsFixer\AbstractFixer;
 use PhpCsFixer\FixerDefinition\CodeSample;
 use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\Preg;
 use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
 use PhpCsFixer\Utils;
@@ -123,7 +124,7 @@ class DocBlocks
      */
     private function fixDocBlock($content, $indent)
     {
-        return ltrim(preg_replace('/^[ \t]*\*/m', $indent.' *', $content));
+        return ltrim(Preg::replace('/^[ \t]*\*/m', $indent.' *', $content));
     }
 
     /**

tests/AutoReview/ProjectCodeTest.php
@@ -14,6 +14,7 @@ namespace PhpCsFixer\Tests\AutoReview;
 
 use PhpCsFixer\DocBlock\DocBlock;
 use PhpCsFixer\Tests\TestCase;
+use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
@@ -252,6 +253,38 @@ final class ProjectCodeTest extends TestCase
         ));
     }
 
+    /**
+     * @dataProvider provideClassesWherePregFunctionsAreForbiddenCases
+     *
+     * @param string $className
+     */
+    public function testThereIsNoPregFunctionUsedDirectly($className)
+    {
+        $rc = new \ReflectionClass($className);
+        $tokens = Tokens::fromCode(file_get_contents($rc->getFileName()));
+        $stringTokens = array_filter(
+            $tokens->toArray(),
+            function (Token $token) {
+                return $token->isGivenKind(T_STRING);
+            }
+        );
+        $strings = array_map(
+            function (Token $token) {
+                return $token->getContent();
+            },
+            $stringTokens
+        );
+        $strings = array_unique($strings);
+        $message = sprintf('Class %s must not use preg_*, it shall use Preg::* instead.', $className);
+        $this->assertNotContains('preg_filter', $strings, $message);
+        $this->assertNotContains('preg_grep', $strings, $message);
+        $this->assertNotContains('preg_match', $strings, $message);
+        $this->assertNotContains('preg_match_all', $strings, $message);
+        $this->assertNotContains('preg_replace', $strings, $message);
+        $this->assertNotContains('preg_replace_callback', $strings, $message);
+        $this->assertNotContains('preg_split', $strings, $message);
+    }
+
     public function provideSrcClassCases()
     {
         return array_map(
@@ -359,6 +392,25 @@ final class ProjectCodeTest extends TestCase
         }
 
         return $data;
+    }
+
+    public function provideClassesWherePregFunctionsAreForbiddenCases()
+    {
+        if (extension_loaded('xdebug') && false === getenv('CI')) {
+            $this->markTestSkipped('Test too slow when Xdebug is loaded.');
+        }
+
+        return array_map(
+            function ($item) {
+                return array($item);
+            },
+            array_filter(
+                $this->getSrcClasses(),
+                function ($className) {
+                    return 'PhpCsFixer\\Preg' !== $className;
+                }
+            )
+        );
     }
 
     private function getSrcClasses()